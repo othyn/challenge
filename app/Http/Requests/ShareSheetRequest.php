27eb6a65075679c59c38fec724a1c4f9a818f583
Ben <git@othyn.com>
2019-08-19 21:16:53 +0100

    Create a new form request for the share sheet, this houses the validation and actions on saving the form

--- app/Http/Requests/ShareSheetRequest.php
+++ app/Http/Requests/ShareSheetRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\ShareSheet;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ShareSheetRequest extends FormRequest
@@ -24,7 +25,25 @@ class ShareSheetRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'customer_name' => 'required|max:255',
+            'friend_name' => 'required|max:255',
+            'friend_email' => 'required|email|max:255',
         ];
+    }
+
+    /**
+     * Save the posted form as a share request.
+     *
+     * @return \App\ShareSheet
+     */
+    public function persist()
+    {
+        return ShareSheet::create(
+            $this->only([
+                'customer_name',
+                'friend_name',
+                'friend_email',
+            ])
+        );
     }
 }
