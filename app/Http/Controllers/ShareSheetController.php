4f86da4042c750a2009bd43e0e39b7840706db77
Ben <git@othyn.com>
2019-08-19 21:16:32 +0100

    Implement resource controller endpoints for loading the create form, alongside storing the saved form

--- app/Http/Controllers/ShareSheetController.php
+++ app/Http/Controllers/ShareSheetController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\ShareSheet;
 use Illuminate\Http\Request;
+use App\Mail\ShareSheetShared;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\ShareSheetRequest;
 
 class ShareSheetController extends Controller
 {
@@ -14,7 +17,7 @@ class ShareSheetController extends Controller
      */
     public function index()
     {
-        //
+        exit('Not required.');
     }
 
     /**
@@ -24,62 +27,72 @@ class ShareSheetController extends Controller
      */
     public function create()
     {
-        //
+        return view('share.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \App\Http\Requests\ShareSheetRequest $request
+     *
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ShareSheetRequest $request)
     {
-        //
+        $shareSheet = $request->persist();
+
+        Mail::to($shareSheet->friend_email)
+            ->send(new ShareSheetShared($shareSheet));
+
+        return redirect()->route('share.create');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\ShareSheet  $shareSheet
+     * @param \App\ShareSheet $shareSheet
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(ShareSheet $shareSheet)
     {
-        //
+        exit('Not required.');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\ShareSheet  $shareSheet
+     * @param \App\ShareSheet $shareSheet
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(ShareSheet $shareSheet)
     {
-        //
+        exit('Not required.');
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\ShareSheet  $shareSheet
+     * @param \Illuminate\Http\Request $request
+     * @param \App\ShareSheet          $shareSheet
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, ShareSheet $shareSheet)
     {
-        //
+        exit('Not required.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\ShareSheet  $shareSheet
+     * @param \App\ShareSheet $shareSheet
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(ShareSheet $shareSheet)
     {
-        //
+        exit('Not required.');
     }
 }
