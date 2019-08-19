d3aafbe3115e19a2e32a65851ae2cff46bfbd5c8
Ben <git@othyn.com>
2019-08-19 20:45:26 +0100

    $ php artisan make:model ShareSheet -a

---
+++ app/Http/Controllers/ShareSheetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ShareSheet;
+use Illuminate\Http\Request;
+
+class ShareSheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ShareSheet  $shareSheet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShareSheet $shareSheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ShareSheet  $shareSheet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ShareSheet $shareSheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ShareSheet  $shareSheet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShareSheet $shareSheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ShareSheet  $shareSheet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShareSheet $shareSheet)
+    {
+        //
+    }
+}
---
+++ database/factories/ShareSheetFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ShareSheet;
+use Faker\Generator as Faker;
+
+$factory->define(ShareSheet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
