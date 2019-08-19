f9c433d385d4b8477e61352c3076b24586f0ad4c
Ben <git@othyn.com>
2019-08-19 20:33:36 +0100

    Add missing read more link onto Page 1

--- resources/views/part_1.blade.php
+++ resources/views/part_1.blade.php
@@ -21,6 +21,9 @@
         <p>Faucibus in ornare quam viverra orci sagittis. Massa id neque aliquam vestibulum morbi blandit cursus risus. Volutpat odio facilisis mauris sit. Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Ut pharetra sit amet aliquam id. Faucibus a pellentesque sit amet porttitor eget. Tincidunt arcu non sodales neque sodales ut etiam. Urna neque viverra justo nec ultrices dui sapien eget. Libero enim sed faucibus turpis. Non odio euismod lacinia at quis risus. Morbi tristique senectus et netus et. Id eu nisl nunc mi.</p>
 
         <p>Quis blandit turpis cursus in hac habitasse platea dictumst quisque. Faucibus pulvinar elementum integer enim neque volutpat. Ullamcorper malesuada proin libero nunc consequat interdum varius sit. Sed euismod nisi porta lorem mollis aliquam. Odio facilisis mauris sit amet massa vitae tortor condimentum. Sit amet dictum sit amet. Sed viverra ipsum nunc aliquet bibendum. Velit ut tortor pretium viverra suspendisse potenti nullam ac tortor. Vulputate dignissim suspendisse in est ante in. Id porta nibh venenatis cras sed felis eget. Vel quam elementum pulvinar etiam non quam lacus. Massa id neque aliquam vestibulum. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque.</p>
+
+        <a id="read-more" href="https://www.getrentr.com/news/" target="_blank" rel="noopener noreferrer">Read more about how we work <span class="red">→</span></a>
+        <!-- No point including something like font awesome just for this... -->
     </div>
 
     <footer>
