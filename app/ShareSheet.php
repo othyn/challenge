bfb2c8f40c0ca11d0f4f47f2e3045aa04805ba3f
Ben <git@othyn.com>
2019-08-19 21:27:11 +0100

    Update the model to allow the fields to be fillable

--- app/ShareSheet.php
+++ app/ShareSheet.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShareSheet extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'customer_name',
+        'friend_name',
+        'friend_email',
+    ];
 }
